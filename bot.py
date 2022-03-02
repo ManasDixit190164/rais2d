052bfabc67a415d5d4158d007155158e45a83bf7
MANAS DIXIT <59383108+ManasDixit190164@users.noreply.github.com>
2022-03-02 20:21:29 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,5 +1,4 @@
 import os
-from distutils.command.clean import clean
 from time import sleep
 from telethon import TelegramClient, events
 
@@ -7,41 +6,25 @@ print("Starting deployment...")
 
 api_id = 8079920
 api_hash = '003d933d631fb5a5abc1053f40480b55'
-
+to_channel = [-1001589471888 ,-1001543401280, -1001717518169, -1001656381315]
+from_channel = -662339025
 client = TelegramClient('session_name', api_id, api_hash)
 
-running = False
-#print
-
-@client.on(events.NewMessage(pattern=r'\.getmsg'))
-async def stophsandler(event):
-    global running
-    running = True
-    to_chat = [-1001589471888 ,-1001543401280, -1001717518169, -1001656381315]
-    c = 0
-    j=0
-    chat = -662339025
-    replied_msg = await event.get_reply_message()
-    msg_id = replied_msg.id
-    await client.delete_messages(chat , event.message)
-    while running:
-        allmsg = await client.get_messages(chat , None , reverse=True , min_id=msg_id , max_id=(msg_id + 5))
-        for msg in allmsg:
-            for i in to_chat:
-                await client.send_message(i , msg)
-            c = c + 1
-            if c == 5:
-                c = 0
-                msg_id = msg_id + 5
-                sleep(3600)
+@client.on(events.NewMessage(incoming=True, chats=from_channel))
+async def _(event):
+    for i in to_channel:
+        time.sleep(0.2)
+        try:
+            await client.send_message(
+                i,
+                event.message
+            )
+        except Exception as e:
+            print(e)
 
 
-@client.on(events.NewMessage(pattern=r'\.stopmsg'))
-async def stophandler(event):
-    global running
-    running = False
-    await client
-    await client.edit_message(event.message, "STOPPED F!!")
+
+
 
 print("Bot has been deployed.")
 client.start()
