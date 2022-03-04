6763662d3e3715dcf73b3d4a6c271f29a72738a7
MANAS DIXIT <59383108+ManasDixit190164@users.noreply.github.com>
2022-03-04 08:26:06 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -7,9 +7,34 @@ api_id = 8079920
 api_hash = '003d933d631fb5a5abc1053f40480b55'
 to_channel = [-1001500045650 ,-1001543401280, -1001773024200, -1001656381315]
 from_channel = -662339025
+x=-1001188182423
+y=-1001384606870
 client = TelegramClient('session_name', api_id, api_hash)
 
 @client.on(events.NewMessage(incoming=True, chats=from_channel))
+async def _(event):
+    for i in to_channel:
+        try:
+            await client.send_message(
+                i,
+                event.message
+            )
+        except Exception as e:
+            print(e)
+            
+            
+@client.on(events.NewMessage(incoming=True, chats=x))
+async def _(event):
+    for i in to_channel:
+        try:
+            await client.send_message(
+                i,
+                event.message
+            )
+        except Exception as e:
+            print(e)
+            
+@client.on(events.NewMessage(incoming=True, chats=y))
 async def _(event):
     for i in to_channel:
         try:
